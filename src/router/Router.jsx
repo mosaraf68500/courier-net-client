a9c282887e071692505b3dbc86a4f5248281b820
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-06-26 12:34:32 +0600

    index setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+
+    {
+        path:"/",
+        element:<div>hello courier net</div>
+    }
+]);
