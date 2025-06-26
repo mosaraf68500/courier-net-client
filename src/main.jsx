fde01da406fcd8faada624a95a5c2704895b0c35
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-06-27 02:11:40 +0600

    aos

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 
 import { RouterProvider } from "react-router";
 import { router } from "./router/Router.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <div className=" bg-[#EAECED]">
-     <RouterProvider router={router} />
-   </div>
+    <div className=" bg-[#EAECED]">
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>
 );
